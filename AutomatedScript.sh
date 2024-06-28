73df53bacaec56013d2c1e384ddc5dffbb1b6f59
ewilliamhally <ewan.hally@lernende.bbw.ch>
2024-06-28 21:02:40 +0200

    erstaufstellung des Projekts & Link zur notion Dok zum README hinzugefügt

--- AutomatedScript.sh
+++ AutomatedScript.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Überprüfung pb ein Programname vorhanden ist
+if [ -z "$1" ]; then
+    echo "Usage: $0 <program_name>"
+    exit 1
+fi
+
+
+program_name=$1
+
+# Logfile path und beispiel log Nachricht
+LOG_FILE="/home/ewanw/abschlussprojekt/M122Nino-Ewan/update_logs.log"
+touch $LOG_FILE
+
+# Function to log messages
+log_message() {
+    local program_name=$1
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - $program_name" >> $LOG_FILE
+}
+
+log_message "Update Prozess abgeschlossen"
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # M122Nino-Ewan
+
+Dokumentation wird auf dieser Notion Seite geführt:
+https://www.notion.so/netcetera-workspac/Version-Control-Script-Project-2eb68f970ee24f65b3af67e1520cc5f8?pvs=4
